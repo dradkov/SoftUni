d8cf2b65323d65854abe853adef4d00ea9df6249
Dimitar Radkov <dimitar_radkov@abv.bg>
2017-08-04 00:19:14 +0300

    ADD Some Tasks

---
+++ OOP C# Course/Reflection/01HarestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,82 @@
+﻿namespace _01HarestingFields
+{
+    using System;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+      public  static void Main(string[] args)
+        {
+            var type = typeof(HarvestingFields);
+
+             
+            var allFields = type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic );
+
+
+            string inputInfo;
+
+            while ((inputInfo = Console.ReadLine()) != "HARVEST")
+            {
+
+
+
+                if (inputInfo == "private")
+                {
+                    foreach (var field in allFields)
+                    {
+                        if (field.IsPrivate)
+                        {
+                            Console.WriteLine($"private {field.FieldType.Name} {field.Name}");
+                        }
+                     
+                    }
+                }
+
+
+                else if (inputInfo == "public")
+                {
+                    foreach (var field in allFields)
+                    {
+                        if (field.IsPublic)
+                        {
+                            Console.WriteLine($"public {field.FieldType.Name} {field.Name}");
+                        }
+
+                    }
+                }
+                else if (inputInfo == "protected")
+                {
+                    foreach (var field in allFields)
+                    {
+                        if (!field.IsPublic && !field.IsPrivate )
+                        {
+                            Console.WriteLine($"protected {field.FieldType.Name} {field.Name}");
+                        }
+
+                    }
+                }
+                else if(inputInfo == "all")
+                {
+                    foreach (var field in allFields)
+                    {
+                        if (!field.IsPublic && !field.IsPrivate)
+                        {
+                            Console.WriteLine($"protected {field.FieldType.Name} {field.Name}");
+                        }
+                        else if (field.IsPublic)
+                        {
+                            Console.WriteLine($"public {field.FieldType.Name} {field.Name}");
+                        }
+                        else if (field.IsPrivate)
+                        {
+                            Console.WriteLine($"private {field.FieldType.Name} {field.Name}");
+                        }
+
+                    }
+                }
+
+
+            }
+        }
+    }
+}
---
+++ OOP C# Course/Reflection/02BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace _02BlackBoxInteger
+{
+    using System;
+
+    class BlackBoxIntegerTests
+    {
+        static void Main(string[] args)
+        {
+
+            var type = typeof(BlackBoxInt);
+
+            BlackBoxInt box = (BlackBoxInt) Activator.CreateInstance(type,true); // Return Default ctor
+
+            //ConstructorInfo ctroBox = type.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic,Type.DefaultBinder,new Type[]{},null );
+
+
+            string inputInfo;
+
+            while ((inputInfo = Console.ReadLine()) != "END")
+            {
+                var split = inputInfo.Split('_');
+                var command = split[0];
+                var intParam = int.Parse(split[1]);
+
+                var method = type.GetMethod(command, BindingFlags.Instance | BindingFlags.NonPublic).Invoke(box, new object[] { intParam });
+
+   
+                var field = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic).First().GetValue(box);
+
+
+                Console.WriteLine(field);
+              
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+    //foreach (var method in methods)
+    //{
+    //    if (method.Name == "Add")
+    //    {
+
+    //    }
+    //}
+
+
+
+
+
+
+
+}
