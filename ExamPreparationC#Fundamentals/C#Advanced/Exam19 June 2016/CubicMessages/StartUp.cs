a68634f9c7220f3aa81442954552306f2dbb9d1f
dradkov <dimitar_radkov@abv.bg>
2017-09-01 23:15:16 +0300

    Add Cubic Task

---
+++ ExamPreparationC#Fundamentals/C#Advanced/Exam19 June 2016/CubicMessages/StartUp.cs
@@ -0,0 +1,63 @@
+﻿
+namespace CubicMessages
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var pattern = @"^(\d+)([a-zA-Z]+)([^a-zA-Z]*)$";
+
+            string inputMessage;
+
+            while ((inputMessage = Console.ReadLine()) != "Over!")
+            {
+                var messageLenght = int.Parse(Console.ReadLine());
+
+                Regex regex = new Regex(pattern);
+
+                Match match = regex.Match(inputMessage);
+
+                if (match.Success)
+                {
+                    string prefix = match.Groups[1].Value;
+                    string message = match.Groups[2].Value;
+                    string ending = String.Empty;
+
+                    if (match.Groups[3].Value != "")
+                    {
+                        ending = match.Groups[3].Value;
+                    }
+
+                    if (message.Length != messageLenght)
+                    {
+                        continue;
+                    }
+
+                    string indexes = Regex.Replace(prefix+ending,@"\D*",string.Empty);
+
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach (var index in indexes)
+                    {
+                        int ind = int.Parse(index.ToString());
+
+                        if (ind >= 0 && ind < messageLenght)
+                        {
+                            sb.Append(message[ind]);
+                        }
+                        else
+                        {
+                            sb.Append(" ");
+                        }
+                    }
+
+                    Console.WriteLine($"{message} == {sb}");
+                }
+            }
+        }
+    }
+}
---
+++ ExamPreparationC#Fundamentals/C#Advanced/Exam19 June 2016/CubicRube/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace CubicRube
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int dimensionSize = int.Parse(Console.ReadLine());
+
+            long sumAllcells = 0;
+            long amountCellSum = 0;
+
+            string inputLine;
+
+            while ((inputLine = Console.ReadLine()) != "Analyze")
+            {
+                int[] token = inputLine.Split().Select(int.Parse).ToArray();
+
+                if (token.Take(3).Any(x => x < 0 || x >= dimensionSize))
+                {
+                    continue;
+                }
+                if (token[3] != 0)
+                {
+                    sumAllcells += token[3];
+                    amountCellSum++;
+                }
+
+            }
+            Console.WriteLine(sumAllcells);
+            Console.WriteLine(Math.Pow(dimensionSize, 3) - amountCellSum);
+        }
+
+
+    }
+}
+
+
+
