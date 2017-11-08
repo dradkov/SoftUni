805782a0aa25d368a33e09adda637c77f0bc945b
Dimitar Radkov <dimitar_radkov@abv.bg>
2017-11-08 11:44:20 +0200

    Upload Introduction to  Entity Framework

---
+++ Databases Advanced - Entity Framework/Introduction to Entity Framework/P02_DatabaseFirst/Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_DatabaseFirst.Data
+{
+    public static class Configuration
+    {
+        public const string ConectionString = "Server=.\\SQLEXPRESS;Database=SoftUni;Integrated Security = True;";
+    }
+}
---
+++ Databases Advanced - Entity Framework/Introduction to Entity Framework/P02_DatabaseFirst/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using P02_DatabaseFirst.Data;
+
+namespace P02_DatabaseFirst
+{
+    using System.Collections.Generic;
+    using P02_DatabaseFirst.Data.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var db = new SoftUniContext();
+
+            using (db)
+            {
+                List<Employee> allEmployees = db.Employees
+                    .OrderBy(e => e.EmployeeId)
+                    .ToList();
+
+                foreach (var em in allEmployees)
+                {
+                    Console.WriteLine($"{em.FirstName} {em.LastName} {em.MiddleName} {em.JobTitle} {em.Salary:f2}");
+                }
+            }
+        }
+
+    }
+}
