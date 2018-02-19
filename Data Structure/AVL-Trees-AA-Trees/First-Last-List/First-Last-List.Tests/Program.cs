b9e42fcea7e49edfb7785a7d0efece9be9cd9c01
Dimitar Radkov <dimitar_radkov@abv.bg>
2018-02-19 14:59:08 +0200

    Add AVL

---
+++ Data Structure/AVL-Trees-AA-Trees/First-Last-List/First-Last-List.Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var items = FirstLastListFactory.Create<int>();
+        items.Add(5);
+        items.Add(10);
+      
+
+
+
+        Console.WriteLine(items);
+    }
+}
---
+++ Data Structure/AVL-Trees-AA-Trees/First-Last-List/First-Last-List/FirstLastList.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+public class FirstLastList<T> : IFirstLastList<T> where T : IComparable<T>
+{
+
+    private LinkedList<T> factory;
+    private OrderedBag<LinkedListNode<T>> ordered;
+    private OrderedBag<LinkedListNode<T>> orderedReversed;
+
+
+    public FirstLastList()
+    {
+        this.factory = new LinkedList<T>();
+        this.ordered = new OrderedBag<LinkedListNode<T>>((x, y) => x.Value.CompareTo(y.Value));
+        this.orderedReversed = new OrderedBag<LinkedListNode<T>>((x,y)=>  -x.Value.CompareTo(y.Value));
+    }
+
+
+    public int Count
+    {
+        get { return this.factory.Count; }
+    }
+
+    public void Add(T element)
+    {
+        LinkedListNode<T> node = new LinkedListNode<T>(element);
+        this.factory.AddLast(node);
+        this.ordered.Add(node);
+        this.orderedReversed.Add(node);
+
+
+    }
+
+    public void Clear()
+    {
+        this.factory.Clear();
+        this.ordered.Clear();
+        this.orderedReversed.Clear();
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+        this.ValidCount(count);
+
+        LinkedListNode<T> current = this.factory.First;
+
+        while (count>0)
+        {
+
+            yield return current.Value;
+            current = current.Next;
+            count--;
+        }
+        
+    } 
+
+    public IEnumerable<T> Last(int count)
+    {
+        this.ValidCount(count);
+
+        LinkedListNode<T> current = this.factory.Last;
+
+        while (count > 0)
+        {
+
+            yield return current.Value;
+            current = current.Previous;
+            count--;
+        }
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        this.ValidCount(count);
+
+        foreach (var item in this.orderedReversed)
+        {
+
+            if (count<=0)
+            {
+                break;
+            }
+            yield return item.Value;
+            count--;
+        }
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+       this.ValidCount(count);
+
+        foreach (var item in this.ordered)
+        {
+
+            if (count <= 0)
+            {
+                break;
+            }
+            yield return item.Value;
+            count--;
+        }
+    }
+
+    public int RemoveAll(T element)
+    {
+        LinkedListNode<T> node = new LinkedListNode<T>(element);
+
+        var range = this.ordered.Range(node, true, node, true);
+
+        foreach (var item in range)
+        {
+            this.factory.Remove(item); 
+        }
+
+        int count = this.ordered.RemoveAllCopies(node);
+        this.orderedReversed.RemoveAllCopies(node);
+
+        return count;
+
+    }
+
+    private void ValidCount(int count)
+    {
+        if (this.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
