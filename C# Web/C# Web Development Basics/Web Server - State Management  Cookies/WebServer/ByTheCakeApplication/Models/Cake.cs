010bdb12715097258a4b6621b9d4efe55d6fb00a
Dimitar Radkov <dimitar_radkov@abv.bg>
2018-05-24 10:42:48 +0300

    State Management Cookies

---
+++ C# Web/C# Web Development Basics/Web Server - State Management  Cookies/WebServer/ByTheCakeApplication/Models/Cake.cs
@@ -0,0 +1,12 @@
+﻿namespace WebServer.ByTheCakeApplication.Models
+{
+   public class Cake
+    {
+     
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ C# Web/C# Web Development Basics/Web Server - State Management  Cookies/WebServer/Server/Http/Contracts/IHttpCookieCollection.cs
@@ -0,0 +1,16 @@
+﻿namespace WebServer.Server.Http.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IHttpCookieCollection : IEnumerable<IHttpCookie>
+    {
+        void Add(IHttpCookie cookie);
+
+        void Add(string key, string value);
+
+        bool ContainsKey(string key);
+
+        IHttpCookie Get(string key);
+
+    }
+}
