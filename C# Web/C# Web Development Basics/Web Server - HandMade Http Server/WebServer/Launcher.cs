0f27488249885e47178ef7f2e03d9d7467b90629
Dimitar Radkov <dimitar_radkov@abv.bg>
2018-05-24 10:37:58 +0300

    Upload Handmade Server

---
+++ C# Web/C# Web Development Basics/Web Server - HandMade Http Server/WebServer/Launcher.cs
@@ -0,0 +1,27 @@
+﻿namespace WebServer
+{
+    using Application;
+    using Server;
+    using Server.Contracts;
+    using Server.Routing;
+
+    public class Launcher : IRunnable
+    {
+        private const int Port = 1337;
+
+        public static void Main()
+        {
+            new Launcher().Run();
+        }
+
+        public void Run()
+        {
+            MainApplication mainApplication = new MainApplication();
+            AppRouteConfig appRouteConfig = new AppRouteConfig();
+            mainApplication.Configure(appRouteConfig);
+
+            WebServer webServer = new WebServer(Port, appRouteConfig);
+            webServer.Run();
+        }
+    }
+}
---
+++ C# Web/C# Web Development Basics/Web Server - HandMade Http Server/WebServer/Server/Handlers/HttpHandler.cs
@@ -0,0 +1,56 @@
+﻿namespace WebServer.Server.Handlers
+{
+    using Common;
+    using Contracts;
+    using Http.Contracts;
+    using Http.Response;
+    using Routing.Contracts;
+    using System.Text.RegularExpressions;
+    using System.Collections.Generic;
+    using Enums;
+
+    public class HttpHandler : IRequestHandler
+    {
+        private readonly IServerRouteConfig serverRouteConfig;
+
+        public HttpHandler(IServerRouteConfig routeConfig)
+        {
+            CoreValidator.ThrowIfNull(routeConfig, nameof(routeConfig));
+
+            this.serverRouteConfig = routeConfig;
+        }
+
+        public IHttpResponse Handle(IHttpContext context)
+        {
+            HttpRequestMethod requestMethod = context.Request.Method;
+            string requestPath = context.Request.Path;
+            var registeredRoutes = this.serverRouteConfig.Routes[requestMethod];
+
+            foreach (var registeredRoute in registeredRoutes)
+            {
+                string routePattern = registeredRoute.Key;
+                IRoutingContext routingContext = registeredRoute.Value;
+
+                Regex routeRegex = new Regex(routePattern);
+                Match match = routeRegex.Match(requestPath);
+
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                IEnumerable<string> parameters = routingContext.Parameters;
+
+                foreach (var parameter in parameters)
+                {
+                    var parameterValue = match.Groups[parameter].Value;
+                    context.Request.AddUrlParameter(parameter, parameterValue);
+                }
+
+                return routingContext.Handler.Handle(context);
+            }
+
+            return new NotFoundResponse();
+        }
+    }
+}
